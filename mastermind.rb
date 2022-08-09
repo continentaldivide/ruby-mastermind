679a750e964f4c058d5a8946c653520eba0978f1
Andrew <andrew@andrewsmith.org>
2022-08-08 22:40:14 -0400

    Make prior guess display optional

--- mastermind.rb
+++ mastermind.rb
@@ -17,7 +17,7 @@ class Mastermind
     computer_create_code
     self.guess_count = 0
     until game_over == true
-      player_guess_code
+      player_turn
       check_guess
       game_over ? show_game_results : show_turn_results
     end
@@ -27,21 +27,27 @@ class Mastermind
     self.code = %w[blue green red yellow]
   end
 
-  def player_guess_code
-    puts 'Please enter your guess.'
-    self.player_guess = sanitize_input
+  def player_turn
+    puts "Please enter your guess. Enter 'history' to see prior guesses."
+    self.player_guess = gets.chomp
+    if player_guess == 'history'
+      show_prior_guesses
+      puts 'Please enter your guess.'
+      self.player_guess = gets.chomp
+    end
+    sanitize_input
     prior_guesses.push(player_guess)
     self.guess_count += 1
   end
 
   def sanitize_input
-    guess_array = gets.chomp.downcase.split(' ')
+    guess_array = player_guess.downcase.split(' ')
     until guess_array.all? { |element| guess_options.include?(element) } && (guess_array.length == 4)
       puts "Sorry, that's not a valid guess. Please provide four colors separated by spaces."
       puts 'Valid colors: red | blue | green | yellow | magenta | cyan'
       guess_array = gets.chomp.downcase.split(' ')
     end
-    guess_array
+    self.player_guess = guess_array
   end
 
   def check_guess
@@ -49,10 +55,17 @@ class Mastermind
     (self.game_over = true) if (player_guess == code) || guess_count == 3
   end
 
+  def show_prior_guesses
+    if guess_count == 0
+      puts 'No guesses yet.'
+    else
+      puts 'Here are your prior guesses:'
+      prior_guesses.each_with_index { |element, index| puts "##{index + 1}: #{element.join(' ')}" }
+    end
+  end
+
   def show_turn_results
     puts 'these are turn results'
-    puts 'here are your prior guesses:'
-    prior_guesses.each_with_index { |element, index| puts "##{index + 1}: #{element.join(' ')}" }
   end
 
   def show_game_results
